b2e404ff4a11cb43c9da8aaa70959a09737c204c
Maxime <maxime.arens@gmail.com>
2019-05-10 08:18:30 +0200

    Finsishing UAL

---
+++ Processeur/Processor/tb_UAL.vhd
@@ -0,0 +1,102 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   11:17:48 05/07/2019
+-- Design Name:   
+-- Module Name:   /home/arens/Documents/SysInfo/PSI/Processeur/Processor/tb_UAL.vhd
+-- Project Name:  Processor
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: UAL
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_UAL IS
+END tb_UAL;
+ 
+ARCHITECTURE behavior OF tb_UAL IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT UAL
+    PORT(
+         A : IN  std_logic_vector(7 downto 0);
+         B : IN  std_logic_vector(7 downto 0);
+         Ctrl : IN  std_logic_vector(2 downto 0);
+         S : OUT  std_logic_vector(7 downto 0);
+         Z : OUT  std_logic;
+         V : OUT  std_logic;
+         C : OUT  std_logic;
+         Nf : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal A : std_logic_vector(7 downto 0) := (others => '0');
+   signal B : std_logic_vector(7 downto 0) := (others => '0');
+   signal Ctrl : std_logic_vector(2 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal S : std_logic_vector(7 downto 0);
+   signal Z : std_logic;
+   signal V : std_logic;
+   signal C : std_logic;
+   signal Nf : std_logic;
+   -- No clocks detected in port list. Replace <clock> below with 
+   -- appropriate port name 
+ 
+   constant clock_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: UAL PORT MAP (
+          A => A,
+          B => B,
+          Ctrl => Ctrl,
+          S => S,
+          Z => Z,
+          V => V,
+          C => C,
+          Nf => Nf
+        );
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- hold reset state for 100 ns.
+      wait for 100 ns;	
+
+      -- insert stimulus here 
+		A <= X"03";
+		B <= X"02";
+		
+		wait for 100 ns;
+		Ctrl <= "011";
+
+      wait;
+   end process;
+
+END;
